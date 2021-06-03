283a9ca2a0b2c7e36fb4bcda1c9d715375905edd
TheRealKevBot <Kglas001@gmail.com>
2021-06-02 18:18:21 -0600

    Build

--- index.js
+++ index.js
@@ -3,7 +3,23 @@
 // Hint: Try converting each value to a Boolean. 
 
 function bouncer(arr) {
-    return arr;
+    let truthy = []
+    let falsey = []
+    arr.map(val => {
+        let bool = Boolean(val)
+        if (bool === true){
+            truthy.push(val)
+        } else {
+            falsey.push(val)
+        }
+    })
+    return truthy
 }
   
-bouncer([7, "ate", "", false, 9]);
+console.log(bouncer([7, "ate", "", false, 9]));
+console.log(bouncer(["a", "b", "c"]));
+console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+console.log(bouncer([0, 0, 0, 0, 0]));
+console.log(bouncer([354, 'NaN', 1, null, undefined]));
+console.log(bouncer(['null', 'NaN', 1, 2, 'undefined']));
+console.log(bouncer([1, 2, 0, -2, -1]));
